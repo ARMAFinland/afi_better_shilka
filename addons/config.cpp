66bee7807c5db6116951d77f6e424234de5e0a82
Johnson <jan_319@msn.com>
2016-02-08 04:12:23 +0200

    Decreased hit and mag reload time

--- addons/config.cpp
+++ addons/config.cpp
@@ -10,7 +10,7 @@ class CfgPatches {
 class CfgAmmo {
 	class B_35mm_AA;
 	class RHS_ammo_23mm_AA: B_35mm_AA {
-		hit = 50;
+		hit = 20;
 		indirectHit = 10;
 		indirectHitRange = 2;
 		airLock = 2;
@@ -21,10 +21,36 @@ class CfgWeapons {
 	class CannonCore;
 	class RHS_weap_AZP23: CannonCore {
 		ballisticscomputer = 1;
-		weaponLockSystem = [8];
-		magazineReloadTime = 10;
+		weaponLockSystem = 8;
+		magazineReloadTime = 3;
 		class manual: CannonCore {
 			dispersion = 0.008;
+		};
+	};
+};
+
+class CfgVehicles {
+	class LandVehicle;
+	class Tank: LandVehicle {
+		class NewTurret;
+	};
+	class Tank_F: Tank {
+		class Turrets {
+			class MainTurret: NewTurret {
+				class Turrets;
+			};
+		};
+	};
+	class APC_Tracked_02_base_F: Tank_F{};
+	class rhs_zsutank_base: APC_Tracked_02_base_F {
+		radarType = 4;
+		lockDetectionSystem = 0;
+		incomingMissileDetectionSystem = 0;
+		class Turrets: Turrets {
+			class MainTurret: MainTurret {
+				maxHorizontalRotSpeed = 2;
+				maxVerticalRotSpeed = 2;
+			};
 		};
 	};
 };
