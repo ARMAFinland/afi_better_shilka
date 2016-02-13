ce8a4a7997551cfc945e92234a1c8ae877948f19
Johnson <jan_319@msn.com>
2016-02-13 13:51:09 +0200

    Removed radar from gunner and driver, Added locking sound for target lock

--- addons/config.cpp
+++ addons/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"rhs_c_heavyweapons","rhs_c_a2port_armor"};
+		requiredAddons[] = {"rhs_c_heavyweapons","rhs_c_a2port_armor","A3_Sounds_F"};
 	};
 };
 
@@ -20,14 +20,12 @@ class CfgAmmo {
 		airLock = 2;
 		cost = 15;
 		model = "\A3\Weapons_f\Data\bullettracer\shell_tracer_red";
-		tracerScale = 1;
+		tracerScale = 0.8;
 		tracerStartTime = 0.1;
 		tracerEndTime = 5;
 		airFriction = -0.000824;
-		muzzleEffect = "";
 		caliber = 1.11;
 		simulation = "shotShell";
-		timeToLive = 10;
 	};
 };
 
@@ -52,12 +50,22 @@ class CfgWeapons {
 		weaponLockSystem = 8;
 		weaponLockDelay = 1;
 		magazineReloadTime = 30;
-		class manual: CannonCore {
-			dispersion = 0.008;
-			reloadTime = 0.03;
-			burst = 4;
-			multiplier= 2;
-		};
+		autoReload = 0;
+		backgroundReload = 0;
+		lockingTargetSound[] = {"\a3\sounds_f\weapons\Rockets\locking_2", 0.5, 0.5};
+		lockedTargetSound[] = {"", 0, 0};
+		cursor = "EmptyCursor";
+		cursorAim = "cannon";
+		showAimCursorInternal = 0;
+		flash = "gunfire";
+		flashSize = 1;
+		shotFromTurret = 0;
+		muzzlePos = "usti hlavne";
+		muzzleEnd = "konec hlavne";
+		selectionFireAnim = "zasleh";
+		fireLightDuration = 0.05;
+		fireLightIntensity = 1;
+
 	};
 };
 
@@ -67,28 +75,38 @@ class CfgVehicles {
 		class NewTurret;
 	};
 	class Tank_F: Tank {
+		class CommanderOptics;
 		class Turrets {
 			class MainTurret: NewTurret {
-				class Turrets;
+				class Turrets {
+					class CommanderOptics;
+				};
 			};
 		};
 	};
 	class APC_Tracked_02_base_F: Tank_F{};
 	class rhs_zsutank_base: APC_Tracked_02_base_F {
 		showAllTargets = 0;
-		radarType = 4;
+		radarType = 4 + 8;
 		lockDetectionSystem = 0;
 		incomingMissileDetectionSystem = 0;
+		gunnerCanSee = 4 + 8 + 16;
+		driverCanSee = 2 + 8 + 16;
 		class Turrets: Turrets {
 			class MainTurret: MainTurret {
 				stabilizedInAxes = 0;
-				magazines[] = {
-					"RHS_mag_AZP23_250"
-				};
+				weapons[] = {"RHS_weap_AZP23"};
+				magazines[] = {"RHS_mag_AZP23_250"};
 				maxHorizontalRotSpeed = 2;
 				maxVerticalRotSpeed = 2;
 				discreteDistance[] = {100};
 				discreteDistanceInitIndex = 0;
+				gunnerOutOpticsShowCursor = 0;
+				gunnerOpticsShowCursor = 0;
+				showaimcursorinternal = 0;
+				class CommanderOptics: CommanderOptics {
+					outGunnerMayFire = 1;
+				};
 			};
 		};
 	};
